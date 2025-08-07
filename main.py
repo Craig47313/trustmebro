1dea47b3a1d2fd3ea589c79b0a1d872c9e3e4680
caleb craig <calebbhatta@icloud.com>
2025-08-08 01:44:39 +0800

    tets

--- main.py
+++ main.py
@@ -24,12 +24,13 @@ raw = pd.concat([raw, makes, drives], axis=1)
 disSTD = np.std(raw['Distance'])
 disMU = np.mean(raw['Distance'])
 raw['Distance zscore'] = raw['Distance'].apply(lambda x : (x-disMU)/disSTD)
-
+print(raw)
 #Defin X,Y & Split
 X = pd.concat([raw['Year'], raw['Distance zscore'], raw['Owner'], makes, drives],axis=1)
 y = raw['Price']
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
+<<<<<<< HEAD
 #Train Model
 rf = RandomForestRegressor(random_state=4)
 rf.fit(X_train, y_train)
@@ -41,7 +42,35 @@ print('Prediction:')
 print(rf.predict(X_train.iloc[[2]]))
 print('Answer')
 print(y_train.iloc[2])
+=======
+from sklearn.linear_model import LinearRegression
 
+rf = LinearRegression()
+rf.fit(X_train, y_train)
+
+
+print('train')
+preds = rf.predict(X_train)
+>>>>>>> b38f236 (tets)
+
+mse = np.mean(pow(preds - y_train, 2))
+
+print(np.sqrt(mse))
+
+nrmse = np.sqrt(mse) / np.mean(y_train)
+
+print('test')
+preds = rf.predict(X_test)
+
+mse = np.mean(pow(preds - y_test, 2))
+
+print(np.sqrt(mse))
+
+nrmse = np.sqrt(mse) / np.mean(y_test)
+
+
+
+print(nrmse)
 #print(X.head(10))
 
 

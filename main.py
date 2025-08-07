7e7fa9e0aa9c703fd73b3228c367c7b140875ffb
ac1311 <paintingsalamander@gmail.com>
2025-08-07 14:23:05 -0400

    Cleaned Code

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ raw = pd.read_csv(path)
 #Remove NaN Values
 raw = raw.dropna()
 print(raw)
+
 #Get Company Name
 raw['make'] = raw['Car Name'].apply(lambda x : x.split(' ')[0])
-#raw['Location'] = raw['Location'].apply(lambda x : x.split('-')[0])
 
 #One-Hot Encoding
 makes = pd.get_dummies(raw.make)
@@ -24,46 +24,27 @@ locs = pd.get_dummies(raw.Location)
 types = pd.get_dummies(raw.Type)
 raw = pd.concat([raw, makes, drives, locs, types, fuels], axis=1)
 
-
-
 #Z-score Normalization
 disSTD = np.std(raw['Distance'])
 disMU = np.mean(raw['Distance'])
 raw['Distance zscore'] = raw['Distance'].apply(lambda x : (x-disMU)/disSTD)
 raw['Age'] = raw['Year'].apply(lambda x : 2025 - x)
 print(raw)
+
 #Defin X,Y & Split
 X = pd.concat([raw['Age'], raw['Distance zscore'], raw['Owner'], locs, makes, drives, types, fuels],axis=1)
 y = raw['Price']
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-
 #Train Model
 rf = RandomForestRegressor(random_state=4)
 rf.fit(X_train, y_train)
-
-print('train')
 preds = rf.predict(X_train)
-
-
 preds = rf.predict(X_test)
 
-
+#Evaluate Model
 kf = KFold(n_splits=5, shuffle=True, random_state=42)
 r2 = cross_val_score(rf, X_test, y_test, cv=kf, scoring='r2')
-
-'''
-mse = np.mean(pow(preds - y_test, 2))
-print(np.sqrt(mse))
-nrmse = np.sqrt(mse) / np.mean(y_test)
-print(nrmse)
-'''
-#print(X.head(10))
-
 print(f'{r2.mean():.4f}(±{r2.std():.4f})')
-
-
-
-
 
 print(''.join(np.repeat('-=+=-<->', 15)))
